2e0cef9a9e2453e3041d181568619a8f33e71d7e
Sites Rapidos <sites.rapidos26@gmail.com>
2026-03-16 15:59:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const sections = document.querySelectorAll("section")
+window.scrollTo(0,0)
+
+const sections = document.querySelectorAll("section:not(.hero)")
+
 const observer = new IntersectionObserver(entries => {
 
 entries.forEach(entry => {
@@ -21,5 +24,17 @@ section.style.transform = "translateY(40px)"
 section.style.transition = "all 0.7s ease"
 
 observer.observe(section)
+
+})
+
+
+/* MENU MOBILE */
+
+const hamburger = document.getElementById("hamburger")
+const nav = document.querySelector("nav")
+
+hamburger.addEventListener("click", () => {
+
+nav.classList.toggle("active")
 
 })
